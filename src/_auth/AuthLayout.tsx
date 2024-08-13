c8a93314a0d10c8cd7965d8187002e167e485f30
josephgoff-git <joeygoff9@gmail.com>
2024-08-13 12:37:00 -0400

    8/13/24 11:30AM 1 hour mark into tutorial

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,21 +1,24 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate } from "react-router-dom";
 
-const AuthLayout = () => {
+export default function AuthLayout() {
   const isAuthenticated = false;
-  
   return (
     <>
-      {isAuthenticated? (
+      {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
         <>
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
